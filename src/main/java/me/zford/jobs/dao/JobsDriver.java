2475a8461246a5fdd06fa65de064416dbdcd06d6
pa001024 <pa001024@qq.com>
2013-05-11 00:21:19 +0800

    Fix COMPILATION ERROR

--- src/main/java/me/zford/jobs/dao/JobsDriver.java
+++ src/main/java/me/zford/jobs/dao/JobsDriver.java
@@ -18,11 +18,9 @@
 
 package me.zford.jobs.dao;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverPropertyInfo;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class JobsDriver implements Driver {
     private Driver driver;
@@ -58,5 +56,10 @@ public class JobsDriver implements Driver {
     @Override
     public boolean jdbcCompliant() {
         return driver.jdbcCompliant();
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
     }
 }
