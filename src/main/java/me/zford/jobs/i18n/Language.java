97829af24e4db31371feac1217a7bb0fa996f9d3
pa001024 <pa001024@qq.com>
2013-05-10 23:20:17 +0800

    Fix non UTF-8 res error

--- src/main/java/me/zford/jobs/i18n/Language.java
+++ src/main/java/me/zford/jobs/i18n/Language.java
@@ -1,51 +1,62 @@
 /**
  * Jobs Plugin for Bukkit
  * Copyright (C) 2011 Zak Ford <zak.j.ford@gmail.com>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package me.zford.jobs.i18n;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.Properties;
 
 public class Language {
-    private static ResourceBundle bundle;
-    
+    private static Properties bundle;
+
     static {
-        bundle = ResourceBundle.getBundle("i18n/messages");
+        bundle = new Properties();
+        try {
+            bundle.load(new InputStreamReader(Language.class.getResourceAsStream("/i18n/messages.properties"), "UTF-8"));
+        } catch (IOException e) {
+        }
     }
-    
-    private Language() { }
-    
+
+    private Language() {
+    }
+
     /**
      * Reloads the config
      */
     public static void reload(Locale locale) {
-        bundle = ResourceBundle.getBundle("i18n/messages", locale);
+        try {
+            bundle.load(new InputStreamReader(Language.class.getResourceAsStream("/i18n/messages_" + locale.toString() + ".properties"), "UTF-8"));
+        } catch (IOException e) {
+        }
     }
-    
+
     /**
      * Get the message with the correct key
+     *
      * @param key - the key of the message
      * @return the message
      */
     public static String getMessage(String key) {
-        return bundle.getString(key);
+        return bundle.get(key).toString();
     }
-    
+
     public static boolean containsKey(String key) {
         return bundle.containsKey(key);
     }
